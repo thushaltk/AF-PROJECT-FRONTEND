6f004bf2bc2a05a35b92f0e790de52deba031cfe
kish_deemu <deemudk@gmail.com>
2021-06-27 23:59:26 +0530

    Modified header to view research papers

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,31 +2,34 @@ import React, { useState } from "react";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
 
-import './Header.css';
+import "./Header.css";
 
 const Header = (props) => {
   let history = useHistory();
 
   const sendAdminLoginTrigger = () => {
     history.push("/admin-login");
-  }
+  };
   const sendReviewerLoginTrigger = () => {
     history.push("/reviewer-login");
-  }
+  };
 
   const userRegistrationNavigator = () => {
     history.push("/new-user");
-  }
+  };
 
   const userLoginNavigator = () => {
     history.push("/user-login");
-  }
+  };
   const sendEditorLoginTrigger = () => {
     history.push("/editor-login");
-  }
+  };
 
   return (
-    <nav style={{padding: "20px 10px", width: "100%"}} className="navbar navbar-light navbar-expand-md custom-header">
+    <nav
+      style={{ padding: "20px 10px", width: "100%" }}
+      className="navbar navbar-light navbar-expand-md custom-header"
+    >
       <div className="container-fluid">
         <div>
           <Link className="navbar-brand" to="/">
@@ -34,13 +37,13 @@ const Header = (props) => {
           </Link>
         </div>
         <button
-            data-toggle="collapse"
-            className="navbar-toggler"
-            data-target="#navbar-collapse"
-          >
-            <span className="sr-only">Toggle navigation</span>
-            <span className="navbar-toggler-icon" />
-          </button>
+          data-toggle="collapse"
+          className="navbar-toggler"
+          data-target="#navbar-collapse"
+        >
+          <span className="sr-only">Toggle navigation</span>
+          <span className="navbar-toggler-icon" />
+        </button>
         <div className="collapse navbar-collapse" id="navbar-collapse">
           <ul className="navbar-nav links">
             <li className="nav-item">
@@ -54,13 +57,18 @@ const Header = (props) => {
               </a>
             </li>
             <li className="nav-item dropdown">
-              <a  className="dropdown-toggle nav-link"
+              <a
+                className="dropdown-toggle nav-link"
                 aria-expanded="false"
                 data-toggle="dropdown"
-                href="#">
+                href="#"
+              >
                 For Authors
               </a>
               <div className="dropdown-menu dropdown-menu-right">
+                <Link className="dropdown-item" to="/papers">
+                  Papers{" "}
+                </Link>
                 <a className="dropdown-item" href="#">
                   Call for Papers
                 </a>
@@ -76,10 +84,12 @@ const Header = (props) => {
               </div>
             </li>
             <li className="nav-item dropdown">
-              <a className="dropdown-toggle nav-link"
+              <a
+                className="dropdown-toggle nav-link"
                 aria-expanded="false"
                 data-toggle="dropdown"
-                href="#">
+                href="#"
+              >
                 Workshops
               </a>
               <div className="dropdown-menu dropdown-menu-right">
@@ -122,7 +132,7 @@ const Header = (props) => {
             <li className="nav-item">
               <a className="nav-link" href="#">
                 {" "}
-               Programs
+                Programs
               </a>
             </li>
           </ul>
@@ -134,19 +144,34 @@ const Header = (props) => {
                 data-toggle="dropdown"
                 href="#"
               >
-                <i style={{fontSize: "30px"}} className="icon ion-android-contacts" />
+                <i
+                  style={{ fontSize: "30px" }}
+                  className="icon ion-android-contacts"
+                />
               </a>
               <div className="dropdown-menu dropdown-menu-right">
-                <button className="dropdown-item" onClick={sendAdminLoginTrigger}>
+                <button
+                  className="dropdown-item"
+                  onClick={sendAdminLoginTrigger}
+                >
                   Admin Login
                 </button>
-                <button className="dropdown-item" onClick={sendEditorLoginTrigger}>
+                <button
+                  className="dropdown-item"
+                  onClick={sendEditorLoginTrigger}
+                >
                   Editor Login
                 </button>
-                <button className="dropdown-item" onClick={sendReviewerLoginTrigger}>
+                <button
+                  className="dropdown-item"
+                  onClick={sendReviewerLoginTrigger}
+                >
                   Reviewer Login
                 </button>
-                <button className="dropdown-item" onClick={userRegistrationNavigator}>
+                <button
+                  className="dropdown-item"
+                  onClick={userRegistrationNavigator}
+                >
                   User Registration
                 </button>
                 <button className="dropdown-item" onClick={userLoginNavigator}>
